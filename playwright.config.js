94fa8b6f17b5ae9a303f9cf91ac07553a60d34c4
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-08-28 17:58:20 +0300

    actions updated to use docker container for e2e tests

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3003',
+    baseURL: 'http://127.0.0.1:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,7 +73,7 @@ module.exports = defineConfig({
   // Run your local dev server before starting the tests
   webServer: {
     command: 'npm run start:test',
-    url: 'http://127.0.0.1:3003',
+    url: 'http://127.0.0.1:3001',
     reuseExistingServer: !process.env.CI,
   },
 })
