d2f334a676184d5a85a178c6f0d785459c89def3
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-08-29 20:09:52 +0300

    actions update to deploy the app

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
